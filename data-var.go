8c190110a9759310acc6ce2e3a69a1bd64e7fac0
tim.chen <tim.chen@ericsson.com.tw>
2019-07-09 10:58:28 +0800

    golang fitst commit

---
+++ data-var.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main(){
+	var text string = "hello"
+	fmt.Println(text)
+	fmt.Println(1)//int
+	fmt.Println(3.41)//float64
+	fmt.Println("hello")//string
+	fmt.Println(true)//bool
+	fmt.Println('a')//rune
+	// fmt.Println('9')
+	// int
+	var a int
+	a = 4
+	fmt.Println(a)
+	var f float64 = 3.2345678345678
+	fmt.Println(f)
+	
+}
---
+++ io.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main(){
+	var a int
+	var b int
+	fmt.Print("please input some number :")
+	fmt.Scanln(&a, &b)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(a+b)
+	// var c int
+	// fmt.Scanln("please input some number : ", &c)
+
+	fmt.Println(a/b)
+}
